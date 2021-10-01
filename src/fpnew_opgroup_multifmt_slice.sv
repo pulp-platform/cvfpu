aceca3bdeea379687e1b13433be4bdd23700ee43
Gianna Paulin <pauling@student.ethz.ch>
2021-10-01 18:03:28 +0200

    Multifmt: fix bitwidth of aux data

--- src/fpnew_opgroup_multifmt_slice.sv
+++ src/fpnew_opgroup_multifmt_slice.sv
@@ -76,7 +76,7 @@ or set Features.FpFmtMask to support only FP32");
   // We will send the format information along with the data
   localparam int unsigned FMT_BITS =
       fpnew_pkg::maximum($clog2(NUM_FORMATS), $clog2(NUM_INT_FORMATS));
-  localparam int unsigned AUX_BITS = FMT_BITS + 3; // also add vectorial and integer flags
+  localparam int unsigned AUX_BITS = FMT_BITS + 4; // also add vectorial and integer flags
 
   logic [NUM_LANES-1:0] lane_in_ready, lane_out_valid, divsqrt_done, divsqrt_ready; // Handshake signals for the lanes
   logic                 vectorial_op;
